cce48db741c6e7d186bd642c16b43b855cad2cfd
Prashant Mishra <prash_mi@yahoo.com>
2020-08-07 23:35:55 +0530

    WIP Code [Has bugs]

--- src/com/leetcode/aug/VerticalTraversal.java
+++ src/com/leetcode/aug/VerticalTraversal.java
@@ -1,10 +1,7 @@
 package com.leetcode.aug;
 
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+//WIP Code [Has bugs]
+import java.util.*;
 
 public class VerticalTraversal {
 
@@ -15,40 +12,69 @@ public class VerticalTraversal {
     }
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> verticalTraversal = new ArrayList<>();
-        Map<Integer, List<Integer>> vMap = new HashMap<>();
+        List<List<Integer>> verticalTraversalRes = new ArrayList<>();
+        Map<Integer, List<NodeXY>> vMap = new HashMap<>();
         MinMax minMax = new MinMax();
-        verticalTraversalHelper(root,0, vMap, minMax );
+        verticalTraversalHelper(root,0,0, vMap, minMax );
         for (int i=minMax.min; i <= minMax.max; i++){
             if(vMap.containsKey(i)){
-                verticalTraversal.add(vMap.get(i));
+                List<NodeXY> verLine =    vMap.get(i);
+
+                Collections.sort(verLine, (Comparator) (o1, o2) -> {
+                NodeXY n1 = (NodeXY)o1;
+                NodeXY n2 = (NodeXY)o2;
+         if(n1.y != n2.y){//different Y values, sort by this
+                            return n1.y - n2.y;
+                        }else {
+                            //order by value
+                            if(n1.x!= n2.x){
+                                return n1.x-n2.x;
+                            }else {//order by
+                                return n1.val-n2.val;
+                            }
+                        }
+                    });
+                //now verLine is sorted, put it verticalTraversalRes
+                List<Integer> ver = new ArrayList<>();
+                for(NodeXY n: verLine){
+                    ver.add(n.val);
+                }
+                verticalTraversalRes.add(ver);
             }
         }
-        return verticalTraversal;
+        return verticalTraversalRes;
     }
 
-  private void  verticalTraversalHelper(TreeNode cur, int vDistance, Map<Integer, List<Integer>> vMap, MinMax minMax ){
+  private void  verticalTraversalHelper(TreeNode cur,int x,  int y,
+                                        Map<Integer, List<NodeXY>> vMap, MinMax minMax ){
         if (cur==null){
             return;
         }
-     //put the current item in vLists
-     if(vMap.containsKey(vDistance)){
-         vMap.get(vDistance).add(cur.val);
+        if(vMap.containsKey(x)){
+         vMap.get(x).add(new NodeXY(x, y, cur.val));
      }else{
-         List<Integer> ls = new ArrayList<>();
-         ls.add(cur.val);
-         vMap.put(vDistance, ls);
+         List<NodeXY> ls = new ArrayList<>();
+         ls.add(new NodeXY(x, y, cur.val));
+         vMap.put(x, ls);
      }
 
-      minMax.min = Math.min(minMax.min, vDistance);
-      minMax.max = Math.max(minMax.max, vDistance);
-      verticalTraversalHelper(cur.left, vDistance-1, vMap, minMax);
-      verticalTraversalHelper(cur.right, vDistance+1, vMap, minMax);
+      minMax.min = Math.min(minMax.min, x);
+      minMax.max = Math.max(minMax.max, x);
+      verticalTraversalHelper(cur.left, x-1, y-1, vMap, minMax);//left is at x-1, y-1
+      verticalTraversalHelper(cur.right, x+1, y-1, vMap, minMax);//right is at x+1, y-1
   }
 
 }
 class MinMax{
     int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+}
+class NodeXY{
+    int x, y, val;
+    NodeXY(int x, int y, int val){
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
 }
 
 class TreeNode {
