dc92c05cf7bb9419b5ac390735569534e1e267a5
Prashant Mishra <inbox.prashant@gmail.com>
2020-09-01 07:47:06 +0530

    match words in a stream

--- src/com/leetcode/medium/DuplicateSubtrees.java
+++ src/com/leetcode/medium/DuplicateSubtrees.java
@@ -1,35 +1,49 @@
 package com.leetcode.medium;
 
 import java.util.*;
+/*
+Input :
+       1
+      / \
+     2   3
+    /   / \
+   4   2   4
+      /
+     4
 
+Output :
+   2
+  /    and    4
+ 4
+Explanation: Above Trees are two duplicate subtrees.
+Therefore, you need to return above trees root in the
+form of a list.
+ */
+
+//O(N^2) time and space
 public class DuplicateSubtrees {
 
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        List<TreeNode> pre = new ArrayList<>();
-        Map<Integer, Integer> locations = new HashMap<>();
-        Set<Integer> dup = new HashSet<>();
         List<TreeNode> res = new ArrayList<>();
-        getPreorder(root, pre);
-
-        for (int i = 0; i < pre.size(); i++){
-            int curVal = pre.get(i).val;
-            if (locations.containsKey(curVal) && !dup.contains(curVal)){
-                dup.add(curVal);//we are going to add a node with this value in solution
-                res.add(pre.get(i));
-            }else {
-                locations.put(curVal, i);
-            }
-        }
+        Map<String, Integer> occurences = new HashMap<>();
+        findDuplicates(root, res, occurences);
         return res;
     }
 
-    private void getPreorder(TreeNode cur, List<TreeNode> pre){
+    private String findDuplicates(TreeNode cur, List<TreeNode> res, Map<String, Integer> occurences){
         if (cur == null){
-            return;
+            return "#";
         }
-        pre.add(cur);
-        getPreorder(cur.left, pre);
-        getPreorder(cur.right, pre);
+        String leftHash = findDuplicates(cur.left, res, occurences);
+        String rightHash = findDuplicates(cur.right, res, occurences);
+        String curHash = cur.val+"-"+leftHash+"-"+rightHash;
+
+        occurences.put(curHash, occurences.getOrDefault(curHash, 0)+1);
+
+        if (occurences.get(curHash) == 2){// this implies that tree @ cur is duplicate and ignores storing duplicates when cur is duplicated more than twice
+            res.add(cur);
+        }
+    return curHash;
     }
 
 }
