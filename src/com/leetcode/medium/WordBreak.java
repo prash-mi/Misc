9aefbb79010a10290b1778519603563c46ec62b7
Prashant Mishra <inbox.prashant@gmail.com>
2020-11-24 21:48:38 +0530

    WordBreak

--- src/com/leetcode/medium/WordBreak.java
+++ src/com/leetcode/medium/WordBreak.java
@@ -5,15 +5,29 @@ import java.util.List;
 import java.util.Set;
 
 public class WordBreak {
+
+    //DP approach, Time (n^3) - two loops and one inner substring call
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict = new HashSet<>();
-        for(String word: wordDict){
-            dict.add(word);
+        Set<String> dict = new HashSet<>(wordDict);
+        boolean[] canBreak = new boolean[s.length()+1];
+        canBreak[0] = true;//you can break an empty string
+
+        for (int wordEnd = 1; wordEnd <= s.length(); wordEnd++){
+            for (int wordStart = 0; wordStart<wordEnd; wordStart++){
+                String currentWord = s.substring(wordStart, wordEnd);
+                if (canBreak[wordStart] && dict.contains(currentWord)){//if we can break the word till wordStart and the currentWord which is between wordStart
+                                                        // and wordEnd is in dictionary then, the word till wordEnd can be broken
+                    canBreak[wordEnd] = true;
+                    break;
+                }
+            }
         }
+        return canBreak[canBreak.length-1];
+    }
 
+    /***Bruitforce approach***/
 
-
-
-        return false;
+    public boolean wordBreakBruitforce(String s, List<String> wordDict) {
+return false;
     }
 }
