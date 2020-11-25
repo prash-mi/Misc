b04d6ccd11d9d0df0a32006a958049fba79f1b4c
Prashant Mishra <inbox.prashant@gmail.com>
2020-11-25 20:17:39 +0530

    WordBreak using BFS and DP

--- src/com/leetcode/medium/WordBreak.java
+++ src/com/leetcode/medium/WordBreak.java
@@ -1,13 +1,40 @@
 package com.leetcode.medium;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class WordBreak {
 
-    //DP approach, Time (n^3) - two loops and one inner substring call
+//very intuitive BFS solution -  Time (n^3), space O(n)
     public boolean wordBreak(String s, List<String> wordDict) {
+        if (s==null||s.length()==0){
+            return false;
+        }
+        Set<String> dictionary = new HashSet<>(wordDict);
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> bfs = new LinkedList<>();
+        bfs.add(0);//start with 0th char
+
+        while (!bfs.isEmpty()){
+            int cur = bfs.poll();
+            if (cur == s.length()){//reached till the end, so a solution is possible
+                return true;
+            }
+            if (visited.contains(cur)){//already visited
+                continue;
+            }
+            for (int end = cur+1; end<= s.length(); end++){
+                String sub = s.substring(cur, end);
+                if (dictionary.contains(sub)){
+                    bfs.add(end);//next word will start at the current end.
+                }
+            }
+            visited.add(cur);
+        }
+        return false;
+    }
+
+    //DP approach, Time (n^3) - two loops and one inner substring call
+    public boolean wordBreakDP(String s, List<String> wordDict) {
         Set<String> dict = new HashSet<>(wordDict);
         boolean[] canBreak = new boolean[s.length()+1];
         canBreak[0] = true;//you can break an empty string
@@ -25,9 +52,7 @@ public class WordBreak {
         return canBreak[canBreak.length-1];
     }
 
-    /***Bruitforce approach***/
 
-    public boolean wordBreakBruitforce(String s, List<String> wordDict) {
-return false;
-    }
+
+
 }
