7723b95a0a53d5b76bc950043c75a460d6b6b26b
Prashant Mishra <inbox.prashant@gmail.com>
2021-01-18 20:58:58 +0530

    PathSum

---
+++ src/com/leetcode/easy/PathSum.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return checkSum(root, sum, 0);
+    }
+    private boolean checkSum(TreeNode cur, int targetSum, int runningSum) {
+        if (cur == null){
+            return false;
+        }
+        if (cur.left == null && cur.right == null && runningSum +cur.val == targetSum){//leaf node
+            return true;
+        }
+
+        boolean left = checkSum(cur.left, targetSum, runningSum+cur.val);
+        boolean right = checkSum(cur.right, targetSum, runningSum+cur.val);
+        if (left||right){//found the target sum
+            return true;
+        }
+        return false;
+    }
+}
